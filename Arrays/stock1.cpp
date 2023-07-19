316a4a8645d03b9b06dcf34afbb0d3b47cbe1a12
Aditya B. Pai <105188352+adityapai1@users.noreply.github.com>
2023-07-19 20:59:23 +0530

    Update stock1.cpp
    Most Optimized version

--- Arrays/stock1.cpp
+++ Arrays/stock1.cpp
@@ -2,35 +2,18 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        int min = *min_element(prices.begin(),prices.end());
-        int m_inx;
-        for(int i =0; i < prices.size();i++){
-            if(prices[i]== min)
-            {
-                m_inx = i;
-                break;
-            }
-        }
+        int minim = INT_MAX;
+        int profit;
+        int maxim = INT_MIN;
+        
+        for (int i = 0 ; i < prices.size(); i++){
 
-        if(m_inx == prices.size()-1){
-            min = *min_element(prices.begin(), prices.end());
-            for(int i =0; i < prices.size()-1;i++){
-            if(prices[i]== min)
-            {
-                m_inx = i;
-                break;
-            }
-        }
-        }
+            minim = min(minim,prices[i]);
+            profit = prices[i] - minim;
+            maxim = max(maxim,profit);
 
-
-        int maxim = min;
-
-        for(int i = m_inx+1 ; i < prices.size();i++){
-            maxim = max(maxim,prices[i]);
         }
-        int profit = maxim-min;
     
-        return profit;
+        return maxim;
     }
 };
