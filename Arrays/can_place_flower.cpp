46f4e7b007a01ef172d658168ba91cc9e4bb502f
Aditya B. Pai <105188352+adityapai1@users.noreply.github.com>
2023-07-25 00:36:11 +0530

    Update can_place_flower.cpp

--- Arrays/can_place_flower.cpp
+++ Arrays/can_place_flower.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        bool flag = false;
+        
+        bool flag ;
+        flowerbed.insert(flowerbed.begin(), 0);
+        flowerbed.push_back(0);
 
-        for(int i = 0 ; i < flowerbed.size(); i++){
+        for(int i  = 1 ;  i<flowerbed.size()-1 ; i++){
 
-            if(flowerbed[i] == 1){
-                continue;
-            }
-            else{
-                if(flowerbed[i+1]==0 && flowerbed[i+2]==0  ){
-                    flowerbed[i+1]= 1;
-                    n--;
-                    if(n == 0 ){
-                        flag = true;
-                        break;
-                    }
-                }
+            if(flowerbed[i-1]== 0 && flowerbed[i]==0 && flowerbed[i+1]==0){
+                flowerbed[i] = 1;
+                n--;
             }
         }
-        
+
+        if(n<=0){
+            flag=true;
+        }
+
+        else{
+            flag= false;
+        }
+
         return flag;
     }
+
 };
